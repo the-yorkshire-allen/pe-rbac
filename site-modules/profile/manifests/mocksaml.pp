a64fd8659faa6cb5a64dc1231d01badd0dc80263
Chris Allen <86972620+the-yorkshire-allen@users.noreply.github.com>
2026-03-24 16:16:09 +0000

    Refactor idp_certificate to use join function

--- site-modules/profile/manifests/mocksaml.pp
+++ site-modules/profile/manifests/mocksaml.pp
@@ -42,21 +42,21 @@ class profile::mocksaml (
   String $display_name = 'MockSAML Test Identity Provider',
   String $idp_sso_url = 'https://mocksaml.com/api/saml/sso',
   String $idp_entity_id = 'https://saml.example.com/entityid',
-  Array[String] $idp_certificate = [
-    'MIIC4jCCAcoCCQC33wnybT5QZDANBgkqhkiG9w0BAQsFADAyMQswCQYDVQQGEwJVUzEPMA0GA1UECgwGQm94eUhR' +
-    'MRIwEAYDVQQDDAlNb2NrIFNBTUwwIBcNMjIwMjI4MjE0NjM4WhgPMzAyMTA3MDEyMTQ2MzhaMDIxCzAJBgNVBAYT' +
-    'AlVTMQ8wDQYDVQQKDAZCb3h5SFExEjAQBgNVBAMMCU1vY2sgU0FNTDCCASIwDQYJKoZIhvcNAQEBBQADggEPADCC' +
-    'AQoCggEBALGfYettMsct1T6tVUwTudNJH5Pnb9GGnkXi9Zw/e6x45DD0RuRONbFlJ2T4RjAE/uG+AjXxXQ8o2SZf' +
-    'b9+GgmCHuTJFNgHoZ1nFVXCmb/Hg8HpdXWnPBrNQfB2zdD6jY8W1S5QkP4yQ8WmSNBjRsVnxkAbMEbZJwqgJzFhS4' +
-    'fY4dY4c0A4qHB4hgFh0rM7Q2wJ8f7w13mJmZ3h5w5V1mCjLrN3g4vOAGXndixaReOiq3EH5XvpMjMkJ3+8+9VYMzM' +
-    'ZOjkgQtAqO36eAFFfNKX7dTj3VpwLkvz6/KFCq8OAwY+AUi4eZm5J57D31GzjHwfjH9WTeX0MyndmnNB1qV75qQR3' +
-    'b2/W5sGHRv+9AarggJkF+ptUkXoLtVA51wcfYm6hILptpde5FQC8RWY1YrswBWAEZNfyrR4JeSweElNHg4NVOs4Tw' +
-    'GjOPwWGqzTfgTlECAwEAATANBgkqhkiG9w0BAQsFAAOCAQEAAYRlYflSXAWoZpFfwNiCQVE5d9zZ0DPzNdWhAybX' +
-    'cTyMf0z5mDf6FWBW5Gyoi9u3EMEDnzLcJNkwJAAc39Apa4I2/tml+Jy29dk8bTyX6m93ngmCgdLh5Za4khuU3AM3' +
-    'L63g7VexCuO7kwkjh/+LqdcIXsVGO6XDfu2QOs1Xpe9zIzLpwm/RNYeXUjbSj5ce/jekpAw7qyVVL4xOyh8AtUW1' +
-    'ek3wIw1MJvEgEPt0d16oshWJpoS1OT8Lr/22SvYEo3EmSGdTVGgk3x3s+A0qWAqTcyjr7Q4s/GKYRFfomGwz0TZ4' +
-    'Iw1ZN99Mm0eo2USlSRTVl7QHRTuiuSThHpLKQQ==',
-  ],
+  Array[String] $idp_certificate = [join([
+        'MIIC4jCCAcoCCQC33wnybT5QZDANBgkqhkiG9w0BAQsFADAyMQswCQYDVQQGEwJVUzEPMA0GA1UECgwGQm94eUhR',
+        'MRIwEAYDVQQDDAlNb2NrIFNBTUwwIBcNMjIwMjI4MjE0NjM4WhgPMzAyMTA3MDEyMTQ2MzhaMDIxCzAJBgNVBAYT',
+        'AlVTMQ8wDQYDVQQKDAZCb3h5SFExEjAQBgNVBAMMCU1vY2sgU0FNTDCCASIwDQYJKoZIhvcNAQEBBQADggEPADCC',
+        'AQoCggEBALGfYettMsct1T6tVUwTudNJH5Pnb9GGnkXi9Zw/e6x45DD0RuRONbFlJ2T4RjAE/uG+AjXxXQ8o2SZf',
+        'b9+GgmCHuTJFNgHoZ1nFVXCmb/Hg8HpdXWnPBrNQfB2zdD6jY8W1S5QkP4yQ8WmSNBjRsVnxkAbMEbZJwqgJzFhS4',
+        'fY4dY4c0A4qHB4hgFh0rM7Q2wJ8f7w13mJmZ3h5w5V1mCjLrN3g4vOAGXndixaReOiq3EH5XvpMjMkJ3+8+9VYMzM',
+        'ZOjkgQtAqO36eAFFfNKX7dTj3VpwLkvz6/KFCq8OAwY+AUi4eZm5J57D31GzjHwfjH9WTeX0MyndmnNB1qV75qQR3',
+        'b2/W5sGHRv+9AarggJkF+ptUkXoLtVA51wcfYm6hILptpde5FQC8RWY1YrswBWAEZNfyrR4JeSweElNHg4NVOs4Tw',
+        'GjOPwWGqzTfgTlECAwEAATANBgkqhkiG9w0BAQsFAAOCAQEAAYRlYflSXAWoZpFfwNiCQVE5d9zZ0DPzNdWhAybX',
+        'cTyMf0z5mDf6FWBW5Gyoi9u3EMEDnzLcJNkwJAAc39Apa4I2/tml+Jy29dk8bTyX6m93ngmCgdLh5Za4khuU3AM3',
+        'L63g7VexCuO7kwkjh/+LqdcIXsVGO6XDfu2QOs1Xpe9zIzLpwm/RNYeXUjbSj5ce/jekpAw7qyVVL4xOyh8AtUW1',
+        'ek3wIw1MJvEgEPt0d16oshWJpoS1OT8Lr/22SvYEo3EmSGdTVGgk3x3s+A0qWAqTcyjr7Q4s/GKYRFfomGwz0TZ4',
+        'Iw1ZN99Mm0eo2USlSRTVl7QHRTuiuSThHpLKQQ==',
+  ], '')],
   Boolean $want_messages_signed = true,
   Boolean $want_assertions_signed = true,
   Boolean $sign_metadata = true,
